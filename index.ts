556a7d44d7d1070bc8d688d8b82a14281b9098f1
Mark Chan <mac101010@gmail.com>
2021-05-11 18:47:20 -0700

    Up to Exercise 9.7
    Added POST endpoint for exercise calculator

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { calculateBMI } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+
+// important for obtaining request body
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
@@ -22,6 +26,31 @@ app.get('/bmi', (req, res) => {
     };
         
     res.send(result);
+});
+
+app.post('/exercises', (req, res) => {
+ 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const  { daily_exercises, target } = req.body;
+
+    if (!daily_exercises || !target)
+        throw new Error('parameters missing');
+
+    if ( isNaN(Number(target)) )
+        throw new Error('malformatted parameters');
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    for (let i = 0; i < daily_exercises.length; i++) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        if ( isNaN(Number(daily_exercises[i])) )
+            throw new Error('malformatted parameters');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const parametersToSend : Array<number> = [ target, ...daily_exercises ];
+
+    res.send(calculateExercises(parametersToSend));
+
 });
 
 const PORT = 3003;
