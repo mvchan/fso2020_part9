e168576cd7adab607f3176dad012e603f0090867
Mark Chan <mac101010@gmail.com>
2021-05-13 14:55:30 -0700

    Up to Exercise 9.9
    Organized patientor backend into separate folder, imported patientor frontend, and installed cors to backend to fix cross-origin resource sharing issue.

--- patientor/patientor-backend/index.ts
+++ patientor/patientor-backend/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
 app.use(express.json());
 
-const PORT = 3000;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
 
 app.get('/ping', (_req, res) => {
     console.log('someone pinged here');
