c591ba69ea913044f765e9228f8e39a0edd8c4ce
Mark Chan <mac101010@gmail.com>
2021-05-21 16:02:23 -0700

    Expanded types.ts for patientor-backend

--- patientor/patientor-backend/src/types.ts
+++ patientor/patientor-backend/src/types.ts
@@ -5,18 +5,25 @@ export interface Diagnose {
     latin?: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Entry {
+}
+
 export interface Patient {
     id: string;
     name: string;
     dateOfBirth: string;
     ssn: string;
-    gender: string;
+    gender: Gender;
     occupation: string;
+    entries: Entry[];
 }
 
 export type RedactedPatient = Omit<Patient, 'ssn'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
+
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
 
 export enum Gender {
     Male = 'male',
