35a1812d9b758ba34e4ec8c56f74fe616be787d2
Mark Chan <mac101010@gmail.com>
2021-05-30 16:53:41 -0700

    Imported Container and Card components for aesthetic

--- patientor/patientor-frontend/src/PatientDetail/index.tsx
+++ patientor/patientor-frontend/src/PatientDetail/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useStateValue } from "../state";
-import { Icon } from "semantic-ui-react";
+import { Icon, Container, Card } from "semantic-ui-react";
 
 const PatientDetail = () => {
 
@@ -22,14 +22,23 @@ const PatientDetail = () => {
             <p>occupation: {patient.occupation}</p>
             <h3>entries</h3>
             <div>{patient.entries.map(entry => 
-                    <div key={entry.id}>
-                        <em>{entry.date} {entry.description}</em>
-                        <ul>
-                            {entry.diagnosisCodes?.map(code => 
-                                <li key={code}>{code} {diagnoses[code]?.name}</li>
-                            )}
-                        </ul>
-                    </div>
+                    <Container key={entry.id}>
+                        <Card.Group>
+                            <Card fluid>
+                                <Card.Content>
+                                    <Card.Header>{entry.date}</Card.Header>
+                                    <Card.Meta>{entry.description}</Card.Meta>
+                                    <Card.Description>
+                                        <ul>
+                                            {entry.diagnosisCodes?.map(code => 
+                                                <li key={code}>{code} {diagnoses[code]?.name}</li>
+                                            )}
+                                        </ul>
+                                    </Card.Description>
+                                </Card.Content>
+                            </Card>
+                        </Card.Group>
+                    </Container>
                 )}
             </div>
         </div>
